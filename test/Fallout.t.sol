39e60ef378e0fafa4eda4b5ab4affdb968188d5f
HoangREALER <hoangle1742002@gmail.com>
2024-09-29 02:04:54 +0700

    Clean

--- test/Fallout.t.sol
+++ test/Fallout.t.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-import "forge-std/Script.sol";
+import "forge-std/Test.sol";
 import "../instance/Fallout.sol";
 
-contract POC is Script {
-    Fallout level2 = Fallout(0xDBDb61eF9B8422f67c2799Cd339840F2ba6f56cd);
+contract Attack is Test {
+    Fallout level2 = Fallout(0x9bd03768a7DCc129555dE410FF8E85528A4F88b5);
 
-    function run() external {
+    function test() external {
         vm.startBroadcast();
 
         console.log("Current Owner is: ", level2.owner()); // querying current owner
